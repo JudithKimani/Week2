6f413eeb7627dc9abeb3cf6c0397156eb89b5249
Judith Kim <nyakiokimani@gmail.com>
2024-01-16 23:52:53 +0300

    ERROR 404

--- trial_2.py
+++ trial_2.py
@@ -21,19 +21,3 @@ elif user_action == "scissors":
         print("Scissors cuts paper! You win!")
     else:
         print("Rock smashes scissors! You lose.")
-
-
-def determine_winner(user_action, computer_action):
-    victories = {
-        Action.Rock: [Action.Scissors],  # Rock beats scissors
-        Action.Paper: [Action.Rock],  # Paper beats rock
-        Action.Scissors: [Action.Paper]  # Scissors beats paper
-    }
-
-    defeats = victories[user_action]
-    if user_action == computer_action:
-        print(f"Both players selected {user_action.name}. It's a tie!")
-    elif computer_action in defeats:
-        print(f"{user_action.name} beats {computer_action.name}! You win!")
-    else:
-        print(f"{computer_action.name} beats {user_action.name}! You lose.")
