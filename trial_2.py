4e47345a93f5571d6eb99e1a518046a974e0ee27
Judith Kim <nyakiokimani@gmail.com>
2024-01-16 23:50:18 +0300

    trials

--- trial_2.py
+++ trial_2.py
@@ -3,7 +3,6 @@ import random
 user_action = input("Enter a choice (rock, paper, scissors): ")
 possible_actions = ["rock", "paper", "scissors"]
 computer_action = random.choice(possible_actions)
-#print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
 
 if user_action == computer_action:
     print(f"Both players selected {user_action}. It's a tie!")
@@ -22,3 +21,19 @@ elif user_action == "scissors":
         print("Scissors cuts paper! You win!")
     else:
         print("Rock smashes scissors! You lose.")
+
+
+def determine_winner(user_action, computer_action):
+    victories = {
+        Action.Rock: [Action.Scissors],  # Rock beats scissors
+        Action.Paper: [Action.Rock],  # Paper beats rock
+        Action.Scissors: [Action.Paper]  # Scissors beats paper
+    }
+
+    defeats = victories[user_action]
+    if user_action == computer_action:
+        print(f"Both players selected {user_action.name}. It's a tie!")
+    elif computer_action in defeats:
+        print(f"{user_action.name} beats {computer_action.name}! You win!")
+    else:
+        print(f"{computer_action.name} beats {user_action.name}! You lose.")
